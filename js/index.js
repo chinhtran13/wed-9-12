42aa0c9606bccc5eca76b54504ab7f4e4a12a21d
chinhtran13 <vienchinh13@gmail.com>
2022-12-11 16:49:42 +0700

    11/12

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const ham = document.querySelector(".hamburger");
 const links = document.querySelector(".nav-links");
+ 
 ham.addEventListener('click', ()=>{
     links.classList.toggle("open")
-    ham.classList.toggle("open")
+    ham.classList.toggle("toggle");
 })
